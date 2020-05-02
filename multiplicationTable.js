25c39e2167ff922ce1bbe5a171ede6bfe3c25f57
Haruna Muasang <harunamuasang@Harunas-Air.lan>
2020-05-02 11:55:52 +0300

    multiplication function

---
+++ multiplicationTable.js
@@ -0,0 +1,15 @@
+
+function multiplication(minValue, maxValue){
+   for (let index = 1; index <= maxValue; index++) {
+       
+       console.log(`${minValue} * ${index} =`, minValue * index);
+    //    console.log(minValue + "*" + index);
+       
+       
+   }
+}
+
+
+multiplication(2, 12)
+
+
